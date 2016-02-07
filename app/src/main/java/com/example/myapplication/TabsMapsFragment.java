2822ee50b1cf4094e36768e0dde1fe15e5a7b70d
Antonio Álvarez Rodrigo <Antonio lvarez Rodrigo>
2016-02-07 15:46:54 +0100

    Tabs Activity. Map. Añadido selector para elegir el tipo de mapa a mostrar. Mejorado el Layout de las infoWindows de los marcadores del mapa. Añadidos algunos permisos al manifest

--- app/src/main/java/com/example/myapplication/TabsMapsFragment.java
+++ app/src/main/java/com/example/myapplication/TabsMapsFragment.java
@@ -1,17 +1,29 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+
 import android.widget.TextView;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,11 +38,14 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
 /**
  * Created by ANTONIO on 02/02/2016.
  */
-public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener, AdapterView.OnItemSelectedListener {
 
     /**
      * El argumento del fragment representa la información de detalle del recurso
@@ -59,11 +74,16 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
      */
     GoogleMap map;
 
+    /**
+     * El selector para el tipo de mapa
+     */
+    private Spinner mapTypeSelector;
+
 
     /**
      * Devuelve una nueva instancia de este FRAGMENT para la página correspondiente
      */
-    public static TabsMapsFragment newInstance( String dataSetName, ArrayList<Resource> resources ) {
+    public static TabsMapsFragment newInstance(String dataSetName, ArrayList<Resource> resources) {
 
         // Crear instancia del FRAGMENT que muestra la lista de recursos
         TabsMapsFragment fragment = new TabsMapsFragment();
@@ -71,7 +91,7 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
         // Le pasamos los argumentos necesarios (en este caso, el detalle del recurso)
         Bundle args = new Bundle();
         args.putParcelableArrayList(ARG_CONTENT_MAP, resources);
-        args.putString(ARG_DATASETNAME, dataSetName );
+        args.putString(ARG_DATASETNAME, dataSetName);
         fragment.setArguments(args);
 
         // Devolver el FRAGMENT
@@ -84,7 +104,6 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
      */
     public TabsMapsFragment() {
     }
-
 
 
     /**
@@ -102,17 +121,21 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
         View rootView = inflater.inflate(R.layout.fragment_results_map, container, false);
 
         // Obtenemos los datos de la lista con los datos que vienen en los argumentos
-        resources = getArguments().getParcelableArrayList( ARG_CONTENT_MAP );
+        resources = getArguments().getParcelableArrayList(ARG_CONTENT_MAP);
 
         //Obtenemos el nombre del dataset cosultado a partir de los argumentos
-        dataSetName = getArguments().getString( ARG_DATASETNAME );
+        dataSetName = getArguments().getString(ARG_DATASETNAME);
 
         // Gets the MapView from the XML layout and creates it
         mapView = (MapView) rootView.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
+        // Show the spinner to select the map type
+        displayMapTypeSelector(rootView);
+
         // Gets to GoogleMap from the MapView and does initialization stuff
-        mapView.getMapAsync( this );
+        mapView.getMapAsync(this);
+
 
 //        map = mapView.getMapAsync( new OnMapReadyCallback() {
 //            @Override public void onMapReady( GoogleMap googleMap ) {
@@ -150,7 +173,6 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
     }
 
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -164,16 +186,17 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        if ( googleMap != null ) {
+        if (googleMap != null) {
             // Indicamos la configuración de la interfaz gráfica
             map.getUiSettings().setAllGesturesEnabled(true);
             map.getUiSettings().setMyLocationButtonEnabled(true);
 
             //Indicamos un Adapter para mostrar InfoWindows personalizadas en el mapa
-            map.setInfoWindowAdapter( new MyInfoWindowAdapter() );
+            map.setInfoWindowAdapter(new MyInfoWindowAdapter());
 
             // Indicamos el centro del mapa
             LatLng center = new LatLng( 39.473995, -6.374444 );
+
 
             // Add the markers to the map (a marker for each resource)
             addMarkersToMap(map, resources, dataSetName);
@@ -240,6 +263,7 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
     }
 
 
+
     class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         private final View myContentsView;
@@ -296,6 +320,70 @@ public class TabsMapsFragment extends Fragment implements OnMapReadyCallback, Go
 
     }
 
+
+    //METODO PARA GESTIONAR EL SPINNER PARA SELECCIONAR EL TIPO DE MAPA
+    private void displayMapTypeSelector( View view ) {
+        //Obtener el spinner del layout
+        mapTypeSelector = (Spinner) view.findViewById( R.id.mapTypeSelector );
+
+        //Rellenar unos datasets de ejemplo
+        List<String> mapTypes = new ArrayList<String>();
+
+        mapTypes.add( "Normal" );
+        mapTypes.add( "Hybrid" );
+        mapTypes.add( "Satélite" );
+        mapTypes.add( "Terrain" );
+
+
+        //Creamos un adaptador
+        ArrayAdapter<String> dropdownDataAdapter = new ArrayAdapter<String>
+                (getContext(), android.R.layout.simple_spinner_item, mapTypes);
+
+        dropdownDataAdapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        //Asignamos el adaptador al spinner
+        mapTypeSelector.setAdapter(dropdownDataAdapter);
+
+        // Spinner item selection Listener
+        mapTypeSelector.setOnItemSelectedListener( this );
+    }
+
+    // Overrided methods of listener for the map type selector
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String mapType = parent.getItemAtPosition( position ).toString();
+
+        switch ( mapType ) {
+            case "Normal":
+                map.setMapType( GoogleMap.MAP_TYPE_NORMAL );
+                break;
+            case "Híbrido":
+            case "Hybrid":
+                map.setMapType( GoogleMap.MAP_TYPE_HYBRID );
+                break;
+            case "Satélite":
+            case "Satellite":
+                map.setMapType( GoogleMap.MAP_TYPE_SATELLITE );
+                break;
+            case "Terreno":
+            case "Terrain":
+                map.setMapType( GoogleMap.MAP_TYPE_TERRAIN );
+                break;
+            default:
+                break;
+        }
+
+        // Showing selected spinner item
+//        Toast.makeText(parent.getContext(), "Selected: " + mapType, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 
 
 

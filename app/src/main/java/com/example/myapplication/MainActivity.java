f8db01d57025a201ec60fc287a7b07937670a2f0
Antonio Álvarez Rodrigo <Antonio lvarez Rodrigo>
2016-02-10 22:39:48 +0100

    Main Activity. Añadido menú navigation drawer para mostrar las opciones de la aplicación. Añadidos algunos iconos.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,8 +4,12 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -41,7 +45,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
+                                                                NavigationView.OnNavigationItemSelectedListener {
 
 
     /**
@@ -64,10 +69,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        super.onCreate( savedInstanceState );
+
+        //Load the layout of the activity
+        setContentView( R.layout.activity_main );
+
+        //Initialize the toolbar
+        Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+
+        //Initialization for the navigation drawer menu
+        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
+        drawer.setDrawerListener( toggle );
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view );
+        navigationView.setNavigationItemSelectedListener( this );
 
 
         //CODIGO NUEVO PARA GESTIONAR EL SPINNER DE LOS DATASETS
@@ -160,6 +179,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         propertiesListView.setOnItemClickListener(new PropertiesListViewOnItemClickListener());
     }
+
 
 
 
@@ -698,5 +718,40 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
+        if ( drawer.isDrawerOpen( GravityCompat.START ) ) {
+            drawer.closeDrawer( GravityCompat.START );
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //Method to execute when the user choose a item in navigation drawer menu
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item ) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_link) {
+            // Handle the camera action
+        } else if (id == R.id.nav_advanced_options) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_about) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+
+    }
+
 
 }
